823a9a38398ad889df16b3f230279ee5a1734db3
0xajing <jing@lorenzo-protocol.xyz>
2024-03-25 15:29:14 +0800

    types:add messageName for fixing rpc-client use

---
+++ types/name.go
@@ -0,0 +1,12 @@
+package types
+
+func (m *ExtensionOptionsWeb3Tx) XXX_MessageName() string {
+	return "ethermint.types.v1.ExtensionOptionsWeb3Tx"
+}
+func (m *ExtensionOptionDynamicFeeTx) XXX_MessageName() string {
+	return "ethermint.types.v1.ExtensionOptionDynamicFeeTx"
+}
+
+func (m *EthAccount) XXX_MessageName() string {
+	return "ethermint.types.v1.EthAccount"
+}
