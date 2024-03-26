96a2e2a3148750948899a5400dedf5ab95ce0ecc
0xajing <jing@lorenzo-protocol.xyz>
2024-03-26 14:56:42 +0800

    add XXX_MessageName to crypto message for fixing registering

---
+++ crypto/ethsecp256k1/name.go
@@ -0,0 +1,9 @@
+package ethsecp256k1
+
+func (m *PubKey) XXX_MessageName() string {
+	return "ethermint.crypto.v1.ethsecp256k1.PubKey"
+}
+
+func (m *PrivKey) XXX_MessageName() string {
+	return "ethermint.crypto.v1.ethsecp256k1.PrivKey"
+}
